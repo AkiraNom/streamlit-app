67f195286bfc46eea7b1bfeb83be7811be1bc96b
Akira Nomura <anomura824@gmail.com>
2024-03-06 10:14:09 +0900

    build: add pre_processing data function for financial and cash flow

--- utils.py
+++ utils.py
@@ -40,35 +40,63 @@ def get_financial_data(ticker_info, document_type:str, period='annual'):
 
         if period == 'annual':
             df = ticker_info.financials
-            df.columns = df.columns.year
+            # pre-processing data structure, data type
+            df = pre_processing_annual_data(df)
 
         else:
             df = ticker_info.quarterly_financials
-            col_names = [f'Q{q} {year}' for q, year in zip(df.columns.quarter, df.columns.year)]
-            df.columns = col_names
+            # pre-processing data structure, data type
+            df = pre_processing_quarterly_data(df)
 
     else: # cash flow data
         if period == 'annual':
             df = ticker_info.cash_flow
-            df.columns = df.columns.year
+            # pre-processing data structure, data type
+            df = pre_processing_annual_data(df)
 
         else:
             df = ticker_info.quarterly_cashflow
-            col_names = [f'Q{q} {year}' for q, year in zip(df.columns.quarter, df.columns.year)]
-            df.columns = col_names
+            # pre-processing data structure, data type
+            df = pre_processing_quarterly_data(df)
 
     return df
+
+def pre_processing_annual_data(df):
+
+    df.columns.name = 'Period'
+    df = df.transpose()
+    df = df.reset_index()
+    df['Period']=df['Period'].dt.year.astype(str)
+    df=df.set_index('Period')
+    df = df.iloc[::-1]
+
+    return df
+
+def pre_processing_quarterly_data(df):
+
+    df.columns.name = 'Period'
+    df = df.transpose()
+    df = df.reset_index()
+    df['Period']=df['Period'].dt.to_period('Q').astype(str).str.replace('Q',' Q')
+    df=df.set_index('Period')
+    df = df.iloc[::-1]
+
+    return df
+
 
 def subset_cash_flow_data(df):
   #Extract variable used in analysis
   prefixes = ['Cash Flow','Free Cash']
-  vars_cat = [var for var in df.index if var.startswith(tuple(prefixes))]
+  vars_cat = [var for var in df.columns if var.startswith(tuple(prefixes))]
 
   #subset a dataset
-  df = df.loc[vars_cat, :]
+  df = df.loc[:,vars_cat]
 
   # shorten the index name
-  df.rename(index=lambda x: x.replace('Cash Flow From Continuing ' ,'').replace('Activities',''), inplace=True)
+  df.rename(columns=lambda x: x.replace('Cash Flow From Continuing ' ,'').replace('Activities',''), inplace=True)
+
+  # set data type as int
+  df = df.astype('Int64')
 
   return df
 
@@ -77,12 +105,17 @@ def subset_financial_data(df):
     vars_cat = ['Total Revenue', 'Net Income Continuous Operations']
 
       #subset a dataset
-    df = df.loc[vars_cat, :]
+    df = df.loc[:,vars_cat]
 
     # rename index
-    df.rename(index=lambda x: x.replace('Net Income Continuous Operations' ,'Earnings').replace('Total',''), inplace=True)
+    df.rename(columns=lambda x: x.replace('Net Income Continuous Operations' ,'Earnings').replace('Total',''), inplace=True)
 
     return df
+
+# get sales data
+def get_sales_data(file_path):
+
+    return pd.read_csv(file_path)
 
 def add_scroll_button(navigation_target: str):
     return st.markdown(f'''
@@ -96,4 +129,15 @@ def add_scroll_button(navigation_target: str):
         </div>
             ''', unsafe_allow_html=True)
 
-
+# navigate to the top
+def add_top_button(navigation_target: str):
+    return st.markdown(f'''
+                    <div class="custom-button">
+            <a target="_self" href="{navigation_target}">
+                <button>
+                    <i class="fas fa-chevron-up"></i>
+                </button>
+            </a>
+            <br>
+        </div>
+            ''', unsafe_allow_html=True)
